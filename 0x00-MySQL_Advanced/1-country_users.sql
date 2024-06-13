34a7a32f420eaf201fa7570136c2df9b3f023231
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-06-12 21:07:12 -0700

    1. In and not out

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
--- create a users table with id, email, name
+-- create a users table with id, email, name, country
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
 	PRIMARY KEY (id),
-	country ENUM("US", "CO", "TN") DEFAULT("US") NOT NULL
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 )
