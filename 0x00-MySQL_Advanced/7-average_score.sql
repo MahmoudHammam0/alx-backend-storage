745fd90ff38fcf89de9f421c3da3981f990bcaf6
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-06-13 01:02:58 -0700

    7. Average score

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,22 @@
+-- creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
+
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(
+	IN user_id INT
+)
+
+BEGIN
+	DECLARE projects_num INT;
+	DECLARE score_sum INT;
+
+	SELECT COUNT(*) INTO projects_num FROM corrections WHERE user_id = user_id;
+	SELECT SUM(score) INTO score_sum FROM corrections WHERE user_id = user_id;
+
+	UPDATE users SET average_score = IF(projects_num = 0, 0, score_sum / projects_num) WHERE users.id = user_id;
+END;
+
+//
+
+DELIMITER ;
