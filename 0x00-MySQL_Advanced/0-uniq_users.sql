0c169c63c1a21981ef8128777d80742288a68f5f
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-06-12 20:52:25 -0700

    0. We are all unique!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- create a users table with id, email, name
-CREATE IF NOT EXISTS TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
