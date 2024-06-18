bc7469b07e52a5c8ece611edcc5fc5bb4a007c48
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-06-18 07:14:26 -0700

    12. Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,28 +3,29 @@
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-db = client.logs
-logs_collection = db.nginx
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    logs_collection = db.nginx
 
-print("{} logs".format(logs_collection.count_documents({})))
+    print("{} logs".format(logs_collection.count_documents({})))
 
-print("Methods:")
+    print("Methods:")
 
-get = logs_collection.count_documents({"method": "GET"})
-print("\tmethod GET: {}".format(get))
+    get = logs_collection.count_documents({"method": "GET"})
+    print("\tmethod GET: {}".format(get))
 
-post = logs_collection.count_documents({"method": "POST"})
-print("\tmethod POST: {}".format(post))
+    post = logs_collection.count_documents({"method": "POST"})
+    print("\tmethod POST: {}".format(post))
 
-put = logs_collection.count_documents({"method": "PUT"})
-print("\tmethod PUT: {}".format(put))
+    put = logs_collection.count_documents({"method": "PUT"})
+    print("\tmethod PUT: {}".format(put))
 
-patch = logs_collection.count_documents({"method": "PATCH"})
-print("\tmethod PATCH: {}".format(patch))
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    print("\tmethod PATCH: {}".format(patch))
 
-delete = logs_collection.count_documents({"method": "DELETE"})
-print("\tmethod DELETE: {}".format(delete))
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    print("\tmethod DELETE: {}".format(delete))
 
-status = logs_collection.count_documents({"method": "GET", "path": "/status"})
-print("{} status check".format(status))
+    status = logs_collection.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(status))
