6149bb7dce47b31b12b600b093cd68688da6a66b
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-06-18 07:53:29 -0700

    15. Log stats - new version

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Log stats """
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    logs_collection = db.nginx
+
+    print("{} logs".format(logs_collection.count_documents({})))
+
+    print("Methods:")
+
+    get = logs_collection.count_documents({"method": "GET"})
+    print("\tmethod GET: {}".format(get))
+
+    post = logs_collection.count_documents({"method": "POST"})
+    print("\tmethod POST: {}".format(post))
+
+    put = logs_collection.count_documents({"method": "PUT"})
+    print("\tmethod PUT: {}".format(put))
+
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    print("\tmethod PATCH: {}".format(patch))
+
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    print("\tmethod DELETE: {}".format(delete))
+
+    status = logs_collection.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(status))
+
+    print("IPs:")
+    pipeline = [
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ]
+    for ip in logs_collection.aggregate(pipeline):
+        print(f"\t{ip['_id']}: {ip['count']}")
